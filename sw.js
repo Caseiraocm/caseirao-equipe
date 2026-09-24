d30fe9877acec36761048f3d67d43051421839b9
Caseiraocm <remersonromario55@gmail.com>
2026-09-24 01:31:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caseirao-equipe-v41';
+const CACHE = 'caseirao-equipe-v42';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
