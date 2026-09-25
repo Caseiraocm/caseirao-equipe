949cb50494a34266a82b3938ce85a697b47a2500
Caseiraocm <remersonromario55@gmail.com>
2026-09-25 01:07:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'caseirao-equipe-launcher-contrast-v34';
+const CACHE = 'caseirao-equipe-print-80mm-v35';
 const APP_SHELL = [
   './', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   './css/base.css', './css/admin.css', './js/config.js', './js/foundation.js',
